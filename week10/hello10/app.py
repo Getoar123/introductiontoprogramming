a5fcbfc6458a2316d1caef17e46fca4d46c9b9b0
Getoar Sopa <getoar.sopa@ibu.edu.mk>
2026-05-09 18:30:52 +0200

    Update app.py

--- week10/hello10/app.py
+++ week10/hello10/app.py
@@ -1,5 +1,3 @@
-# Moves default value to template
-
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
@@ -7,6 +5,9 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
+    name = request.args.get("name")
+
     if request.method == "POST":
-        return render_template("greet.html", name=request.form.get("name"))
-    return render_template("index.html")
+        name = request.form.get("name")
+
+    return render_template("index.html", name=name)
