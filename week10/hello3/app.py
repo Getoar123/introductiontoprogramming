330353a0670339b5f88eaa8a549d4fd3ed08efff
Getoar Sopa <getoar.sopa@ibu.edu.mk>
2026-05-09 15:37:56 +0200

    Update app.py

--- week10/hello3/app.py
+++ week10/hello3/app.py
@@ -1,5 +1,3 @@
-# Says hello to request.args["name"]
-
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
@@ -7,8 +5,5 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    if "name" in request.args:
-        name = request.args["name"]
-    else:
-        name = "world"
+    name = request.args.get("name", "world")
     return render_template("index.html", placeholder=name)
