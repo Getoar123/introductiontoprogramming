5f77c67ef2e41a769df315f8874bf6693e173bb1
Getoar Sopa <getoar.sopa@ibu.edu.mk>
2026-05-09 18:29:47 +0200

    Update app.py

--- week10/hello9/app.py
+++ week10/hello9/app.py
@@ -1,5 +1,3 @@
-# Uses a single route
-
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
@@ -7,6 +5,21 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
+    # default value
+    name = "world"
+
+    # if form submitted (POST)
     if request.method == "POST":
-        return render_template("greet.html", name=request.form.get("name", "world"))
-    return render_template("index.html")
+        name = request.form.get("name", "world")
+
+    # if query string used (GET)
+    elif request.method == "GET" and request.args.get("name"):
+        name = request.args.get("name")
+
+    return render_template("index.html", name=name)
+
+
+@app.route("/greet", methods=["POST"])
+def greet():
+    name = request.form.get("name", "world")
+    return render_template("greet.html", name=name)
